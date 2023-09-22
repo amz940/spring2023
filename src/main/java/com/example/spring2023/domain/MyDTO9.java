04f8069f0f6ed79cab04cb253abe848ff7d69f3f
dh_bug <dh_bug@naver.com>
2023-09-22 12:41:22 +0900

    java beans

---
+++ src/main/java/com/example/spring2023/domain/MyDTO9.java
@@ -0,0 +1,12 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDTO9 {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String className;
+    private String phoneNumber;
+}
