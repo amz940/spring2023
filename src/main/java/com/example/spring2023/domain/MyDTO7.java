74845b179f62a21037bb0582403427f59f99187a
dh_bug <dh_bug@naver.com>
2023-09-22 12:41:14 +0900

    property

---
+++ src/main/java/com/example/spring2023/domain/MyDTO7.java
@@ -0,0 +1,23 @@
+package com.example.spring2023.domain;
+
+public class MyDTO7 {
+    public String getAddress() {
+        return "서울";
+    }
+    public Integer getAge() {
+        return 99;
+    }
+
+    public String getEmail() {
+        return "abc@naver.com";
+    }
+
+    public String getName() {
+        return "김봉석";
+    }
+
+    @Override
+    public String toString() {
+        return "내가만든 클래스😀";
+    }
+}
---
+++ src/main/java/com/example/spring2023/domain/MyDTO8.java
@@ -0,0 +1,9 @@
+package com.example.spring2023.domain;
+import lombok.Data;
+
+@Data
+public class MyDTO8 {
+    private String food;
+    private String birthDate;
+    private String computer;
+}
