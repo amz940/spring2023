e57f02be04dca8592067e2f42adbe19725f729ad
dh_bug <dh_bug@naver.com>
2023-09-22 12:40:24 +0900

    Controller

--- src/main/java/com/example/spring2023/controller/Controller04.java
+++ src/main/java/com/example/spring2023/controller/Controller04.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("main4")
-public class Controller4 {
+public class Controller04 {
     // main4/sub1?name=son
     // request parameter의 이름과 method parameter의 이름이 같으면
     // @RequestParam의 value element 생략 가능 하다
