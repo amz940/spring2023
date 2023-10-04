1b9fd95608888054d3d1a22b9c00457d68ee2cc0
dh_bug <dh_bug@naver.com>
2023-10-04 17:13:18 +0900

    select

---
+++ sql/01sql.sql
@@ -0,0 +1,12 @@
+-- 주석
+# 주석
+-- 실행이 잘 됬으면 체크 표시, 안됬으면 x 표시
+select * from w3schools.customers;
+select * from w3schools.employees;
+select *
+from w3schools.customers;
+
+-- sql은 대소문자 구분을 안한다
+SELECT * FROM W3SCHOOLS.CUSTOMERS;
+-- 키워드는 대문자,  나머지는 소문자로 쓰는 관습이 있다
+SELECT * FROM w3schools.customers;
