81d7f5ba0f116293c82a4e9e5829198c5ef5a40c
dh_bug <dh_bug@naver.com>
2023-10-04 17:14:30 +0900

    javaBeans

---
+++ src/main/java/com/example/spring2023/domain/MyDto16.java
@@ -0,0 +1,11 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDto16 {
+    private String id;
+    private String name;
+    private String unit;
+    private Integer price;
+}
