977c41bed7ded27ff7a85fafaf1a72aeff469942
dh_bug <dh_bug@naver.com>
2023-10-12 16:57:49 +0900

    공급자 입력

---
+++ src/main/java/com/example/spring2023/domain/MyDto18Supplier.java
@@ -0,0 +1,14 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDto18Supplier {
+    private String supplierName;
+    private String contactName;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+    private String phone;
+}
