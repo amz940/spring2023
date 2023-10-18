e8bad0b64327e50428220ae5c28c4d9f9247237b
dh_bug <dh_bug@naver.com>
2023-10-18 17:12:05 +0900

    insert

---
+++ src/main/java/com/example/spring2023/domain/MyDto31.java
@@ -0,0 +1,9 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDto31 {
+    private String name;
+    private String country;
+}
---
+++ src/main/java/com/example/spring2023/domain/MyDto32.java
@@ -0,0 +1,11 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class MyDto32 {
+    private String lastName;
+    private String firstName;
+}
