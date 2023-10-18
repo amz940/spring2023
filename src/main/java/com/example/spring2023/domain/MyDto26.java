2f657e21ff5b230c81eac1d69e261c4b079281a2
dh_bug <dh_bug@naver.com>
2023-10-18 17:11:39 +0900

    여러 자바빈

---
+++ src/main/java/com/example/spring2023/domain/MyDto26.java
@@ -0,0 +1,10 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDto26 {
+    private String name;
+    private String country1;
+    private String country2;
+}
