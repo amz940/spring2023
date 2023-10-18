14f745018e6fe190dd4d124806318a74d01a1153
dh_bug <dh_bug@naver.com>
2023-10-18 17:10:55 +0900

    여러행 여러열, javabean 사용

---
+++ src/main/java/com/example/spring2023/domain/MyDto23.java
@@ -0,0 +1,13 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDto23 {
+    private String date;
+    private String name;
+    private String cname;
+    private Integer quantity;
+    private Double price;
+
+}
