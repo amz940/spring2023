f0e410556a30484d8a78f590542d001aaa559ae0
dh_bug <dh_bug@naver.com>
2023-10-19 17:41:31 +0900

    update

--- src/main/java/com/example/spring2023/controller/Controller30.java
+++ src/main/java/com/example/spring2023/controller/Controller30.java
@@ -4,9 +4,11 @@ import com.example.spring2023.dao.MyDao4;
 import com.example.spring2023.domain.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -106,4 +108,75 @@ public class Controller30 {
       int row = dao.insert2(emp);
         System.out.println("row = " + row);
     }
+
+    // 삭제하는 코드
+    @GetMapping("sub11")
+    public void method11(Integer id){
+        int delete1 = dao.delete1(id);
+        System.out.println("delete1 = " + delete1 + "개의 행이 지워짐");
+    }
+
+    // 3번 상품이 삭제되는 메소드
+    // /main30/sub12?pid= 3
+    @GetMapping("sub12")
+    public void method12(Integer pid){
+        int delete2 = dao.delete2(pid);
+        System.out.println(delete2 + "개의 행이 삭제됨");
+    }
+
+    // update 하는 코드
+    // main30/sub13?id=?
+    @GetMapping("sub13")
+    public void method13(Integer id, Model model){
+        // 직원 조회
+        MyDto33Employee employee = dao.select8(id);
+
+        model.addAttribute("employee", employee);
+    }
+
+    @PostMapping("sub14")
+    public String method14(MyDto33Employee employee, RedirectAttributes rttr){
+        // 직원 수정
+        int update1 = dao.update1(employee);
+
+        if( update1 == 1){
+            // 모델에 값을 넣는 과정
+            rttr.addFlashAttribute("message", "정보가 수정되었습니다");
+        } else {
+            rttr.addFlashAttribute("message", "정보가 수정되지 않았습니다");
+        }
+        // queryString에 값을 넣는 코드
+        rttr.addAttribute("id", employee.getId());
+
+        return "redirect:/main30/sub13";
+    }
+
+    // /main30/sub15?id=?
+    // 3번 고객 조회 -> view로 포워딩(전송)
+
+    // post /main30/sub16으로 받아서 정보 수정 후
+    // /main30/sub15?id=? 로 redirect 해주기
+    @GetMapping("sub15")
+    public void method15(Integer id, Model model){
+        MyDto34Customer customer = dao.select9(id);
+
+        model.addAttribute("customer", customer);
+    }
+
+    @PostMapping("sub16")
+    public String method16(MyDto34Customer customer, RedirectAttributes rttr){
+        int update1 = dao.update2(customer);
+
+        if( update1 == 1){
+            // view를 볼 때마다 초기화 되는 값을 넣는것, 일시적인, model에 들어가는 값
+            rttr.addFlashAttribute("message", "정보가 수정됨");
+        } else{
+            rttr.addFlashAttribute("message", "정보가 수정되지 않음");
+        }
+        // 이 코드를 작성함으로 써 /main30/sub15?id=? 에서 id=? 값을 넣어줄수 있다 , 쿼리에 들어가는 값
+        rttr.addAttribute("id",customer.getId());
+
+        return "redirect:/main30/sub15";
+    }
+
 }
