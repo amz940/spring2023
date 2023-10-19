81386ec7634de45955ecb8749c889a9e4eab7e4a
dh_bug <dh_bug@naver.com>
2023-10-19 17:41:52 +0900

    update

--- src/main/java/com/example/spring2023/dao/MyDao4.java
+++ src/main/java/com/example/spring2023/dao/MyDao4.java
@@ -1,9 +1,7 @@
 package com.example.spring2023.dao;
 
 import com.example.spring2023.domain.*;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,7 +12,7 @@ public interface MyDao4 {
     @Select("""
         SELECT CustomerName
         FROM customers
-        WHERE CustomerID = #{아이디}
+        WHERE CustomerID = #{id}
     """)
     // 파라미터가 하나면 이름이 일치하지 않아도 된다
     // 단 복수 일 경우 꼭 일치해야 한다
@@ -71,6 +69,7 @@ public interface MyDao4 {
     """)
     List<String> select7(MyDto29 dto1, MyDto30 dto2);
 
+    // 삽입 하는 코드
     @Insert("""
         INSERT INTO customers (CustomerName, Country)
         VALUE (#{name}, #{country})
@@ -82,4 +81,63 @@ public interface MyDao4 {
         VALUE (#{lastName}, #{firstName})
     """)
     int insert2(MyDto32 emp);
+
+    // 삭제하는 코드
+    @Delete("""
+        DELETE FROM customers
+        WHERE CustomerID = #{id}
+    """)
+    int delete1(Integer id);
+
+    @Delete("""
+        DELETE FROM products
+        WHERE ProductID = #{pid}
+    """)
+    int delete2(Integer pid);
+
+    @Select("""
+        SELECT employeeid id,
+               lastname, firstname, birthdate, photo, notes
+        FROM employees
+        WHERE EmployeeID = #{id}
+    """)
+    MyDto33Employee select8(Integer id);
+
+    // 수정 하는 코드
+    @Update("""
+        UPDATE employees
+        SET LastName = #{lastName},
+            FirstName = #{firstName},
+            Photo = #{photo},
+            Notes = #{notes},
+            BirthDate = #{birthDate}
+        WHERE EmployeeID = #{id}
+    """)
+    int update1(MyDto33Employee employee);
+
+    @Select("""
+        SELECT customerid id,
+               customername name,
+               contactname, address, city, postalcode, country
+        FROM customers
+        WHERE CustomerID = #{id}
+    """)
+    MyDto34Customer select9(Integer id);
+
+    // #{} 안에 들어가 있는 이름이 view랑 같아야한다
+    // sql 안엔 왠만해선 주석 작성 하지 마라 , 비권장
+    @Update("""
+        UPDATE customers
+        SET CustomerName = #{name},
+            ContactName = #{contactName},
+            Address = #{address},
+            City = #{city},
+            PostalCode = #{postalCode},
+            Country = #{country}
+        WHERE CustomerID = #{id}
+    """)
+    int update2(MyDto34Customer customer);
+
+
+
 }
