8229e06965394b3b02d003f6a6cd1876fdb82f2b
dh_bug <dh_bug@naver.com>
2023-10-20 12:31:46 +0900

    활용

---
+++ src/main/java/com/example/spring2023/domain/MyDto38.java
@@ -0,0 +1,10 @@
+package com.example.spring2023.domain;
+
+import lombok.Data;
+
+@Data
+public class MyDto38 {
+    private Integer eid;
+    private String lastName;
+    private String firstName;
+}
